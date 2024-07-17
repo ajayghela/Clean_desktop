8fcab67d7e77c19c051180e95a25339a2335a3b2
ajayghela <ajay_ghela@hotmail.com>
2024-07-17 13:52:48 +0100

    desktop_items refactored

--- Clean_desktop.py
+++ Clean_desktop.py
@@ -3,7 +3,16 @@ from datetime import date
 
 
 def desktop_items():
-    print(os.listdir("/Users/ajay/Desktop"))
+    files = os.listdir("/Users/ajay/Desktop")
+    correct_files = [item for item in files if item[-4:] == '.png' or item[-4:] == '.mov']
+    #print(files)
+    #for item in files:
+       # if item[-4:] == '.png':
+        #    correct_files.append(item)
+        #elif item[-4:] == '.mov':
+        #    correct_files.append(item)
+    print(correct_files)
+    
 
 def create_destination_folder():
     directory_name = date.today().strftime('%Y-%m-%d')
@@ -14,4 +23,4 @@ def create_destination_folder():
     os.mkdir(path)
     print("Directory '%s' created" %directory)
     
-create_destination_folder()
+desktop_items()
