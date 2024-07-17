3499cb693fe2fbfbd4033af9bf3f44216432e5ac
ajayghela <ajay_ghela@hotmail.com>
2024-07-17 11:46:38 +0100

    Create destination folder with time removed from the folder name

--- Clean_desktop.py
+++ Clean_desktop.py
@@ -1,24 +1,17 @@
 import os
-from datetime import datetime
+from datetime import date
 
 
 def desktop_items():
     print(os.listdir("/Users/ajay/Desktop"))
 
 def create_destination_folder():
-    directory_name = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    directory_name = date.today().strftime('%Y-%m-%d')
     #print(directory_name)
-
     directory = directory_name
     parent_dir = "/Users/ajay/Desktop/"
     path = os.path.join(parent_dir, directory)
     os.mkdir(path)
     print("Directory '%s' created" %directory)
-
-
-
-#def rename_destination_folder()
-
     
-
 create_destination_folder()
