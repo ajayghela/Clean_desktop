d897afd7f206ae946a36a7e17da22fd521fab2da
ajayghela <ajay_ghela@hotmail.com>
2024-07-24 12:14:55 +0100

    Fixed the moving_items() function, created destination_path variable and passed it through the create_destination_folder() and moving_items() functions

--- Clean_desktop.py
+++ Clean_desktop.py
@@ -11,17 +11,20 @@ def path():
 def create_destination_folder(path):
     #add if statement to the check if there actually dekstop items and for the folder
     os.mkdir(path)
-    print("Directory '%s' created" %directory)
-
+    print("Directory '%s' created" %path)
     #error if folder has already been created
 
 def moving_items(path):
-    files = os.listdir("/Users/ajay/Desktop")
+    desktop_path = "/Users/ajay/Desktop/"
+    files = os.listdir(desktop_path)
     print(files)
     for item in files: 
        if item[-4:] == '.png' or item[-4:] == '.mov':
            #print(item)
-            os.path.join(path, item)
+            source = os.path.join(desktop_path, item)
+            destination = os.path.join(path, item)
+            os.rename(source, destination)
+            print(f"Moved '{item}' to '{path}'")
     #correct_files = [item for item in files if item[-4:] == '.png' or item[-4:] == '.mov']
     #file_full_path = [os.path.join(desktop_path, item) for item in correct_files] 
     #return file_full_path
@@ -33,6 +36,6 @@ def moving_items(path):
         #elif item[-4:] == '.mov':
         #    correct_files.append(item)
 
-path = path()
-create_destination_folder()
-moving_items()
+destination_path = path()
+create_destination_folder(destination_path)
+moving_items(destination_path)
